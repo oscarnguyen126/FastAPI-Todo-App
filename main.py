c543ca29b03d36098afe0b0590b6cea515842a8e
trau <oscarnguyen126@gmail.com>
2023-08-22 05:02:25 +0700

    CRUD function

---
+++ main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import List
+from fastapi.exceptions import HTTPException
+
+
+class Todo(BaseModel):
+    name: str
+    is_done: bool = Field(default=False)
+
+app = FastAPI()
+
+store_todos = []
+
+@app.get('/')
+async def home():
+    return {"Welcome"}
+
+@app.post('/todos/')
+async def create_todo(todo: Todo):
+    store_todos.append(todo)
+    return todo
+
+
+@app.get('/todos/', response_model=List[Todo])
+async def get_all_todos():
+    return store_todos
+
+
+@app.get('/todos/{id}')
+async def get_todo(id: int):
+    try:
+        return store_todos[id]
+    except:
+        raise HTTPException(status_code=404, detail="Todo not found")
+
+
+@app.put('/todos/{id}')
+async def update_todo(id: int, todo: Todo):
+    try:
+        store_todos[id] = todo
+        return store_todos[id]
+    except:
+        raise HTTPException(status_code=404, detail="Todo not found")
+
+
+@app.delete('/todos/{id}')
+async def delete_todo(id: int):
+    try:
+        obj = store_todos[id]
+        store_todos.pop(id)
+        return obj
+    except:
+        raise HTTPException(status_code=404, detail="Todo not found")
