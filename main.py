ba9e9d5d31d82461b93436ec682385b4d83c87a3
trau <oscarnguyen126@gmail.com>
2023-08-23 05:13:42 +0700

    Oauth

--- main.py
+++ main.py
@@ -1,69 +1,187 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel, Field
-from typing import List
-from fastapi.exceptions import HTTPException
 from deta import Deta
 from dotenv import load_dotenv
-import os
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from typing import Annotated
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+
+
+SECRET_KEY = "06db48f32c4ed07b50601d316b164bd99262a0820c4e122cac68f3922dfb503d"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
 
 
 load_dotenv()
 
 app = FastAPI()
 
-PROJECT_KEY = os.environ.get('DETA_PROJECT_KEY')
-deta = Deta(PROJECT_KEY)
-db = deta.Base("Todos")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+deta = Deta("c0vxprmyzwm_2wuD5rPtTRHQ2Z3zBTh2qTUAS8P9f8Xq")
+
+db = deta.Base("todos")
+
+users = deta.Base("users")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+
+    class Config:
+        orm_mode: True
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 class Todo(BaseModel):
     name: str
     is_done: bool = Field(default=False)
 
-store_todos = []
+    class Config:
+        orm_mode: True
+
 
 @app.get('/')
-async def home():
+async def home(token: Annotated[str, Depends(oauth2_scheme)]):
     return {"Welcome"}
 
-@app.post('/todos/')
-async def create_todo(todo: Todo):
-    store_todos.append(todo)
-    return todo
+
+def fake_decode_token(token):
+    user = get_user(fake_users_db, token)
+    return user
 
 
-@app.get('/todos/', response_model=List[Todo])
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
+
+
+@app.get('/todos/')
 async def get_all_todos():
     return db.fetch()
 
 
 @app.post('/todos/')
 async def create_todo(todo: Todo):
-    return db.put(todo.dict())
+    return db.put(todo.__dict__)
 
 
-@app.get('/todos/{id}')
-async def get_todo(id: int):
-    try:
-        return store_todos[id]
-    except:
-        raise HTTPException(status_code=404, detail="Todo not found")
+@app.get('/todos/{key}')
+async def get_todo(key: str):
+    return db.get(key)
 
 
-@app.put('/todos/{id}')
-async def update_todo(id: int, todo: Todo):
-    try:
-        store_todos[id] = todo
-        return store_todos[id]
-    except:
-        raise HTTPException(status_code=404, detail="Todo not found")
+@app.delete('/todos/{key}')
+async def delete_todo(key: str):
+    db.delete(key)
+    return {"Todo removed"} 
 
 
-@app.delete('/todos/{id}')
-async def delete_todo(id: int):
-    try:
-        obj = store_todos[id]
-        store_todos.pop(id)
-        return obj
-    except:
-        raise HTTPException(status_code=404, detail="Todo not found")
+@app.put('/todos/{key}')
+async def update_todo(todo: Todo, key: str):
+    db.update(todo.__dict__, key)
+    return {"Todo with the key " + key + " is updated"}
